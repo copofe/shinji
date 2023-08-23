ae87355b1c80038555166270fe1c2b09e4e62dad
copofe <me@shinji.me>
2023-08-23 17:13:32 +0800

    style

--- src/app/(main)/memo/page.tsx
+++ src/app/(main)/memo/page.tsx
@@ -23,7 +23,7 @@ export default async function Memo() {
   const res = await fetch(`${memosApi}?openId=${openId}`)
   const memos: Memo[] = await res.json()
   return (
-    <div className="flex-1 self-center flex items-stretch flex-col w-[42rem] mt-8 lg:mt-12">
+    <div className="flex-1 self-center flex items-stretch flex-col w-full p-2 lg:p-0 lg:w-[42rem] mt-8 lg:mt-12">
       {memos.map((memo) => {
         return (
           <div
