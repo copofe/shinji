4602d53ede18ab82f9b8229572f2ad4efaecdba5
copofe <me@shinji.me>
2023-08-23 11:30:41 +0800

    feat: minimal login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,97 +1,35 @@
 'use client'
 
-import * as z from 'zod'
-import { useForm } from 'react-hook-form'
-import {
-  Button,
-  Input,
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-} from '::/components/ui'
-import { useState } from 'react'
+import { useEffect } from 'react'
+import { useTheme } from 'next-themes'
 import { useRouter } from 'next/navigation'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-
-const formSchema = z.object({
-  email: z
-    .string({
-      required_error: 'error',
-    })
-    .email(),
-  password: z.string(),
-})
 
 export default function Login() {
   const supabase = createClientComponentClient()
-  const form = useForm<z.infer<typeof formSchema>>({
-    defaultValues: {
-      email: undefined,
-      password: undefined,
-    },
-  })
-
-  const [loading, setLoading] = useState(false)
+  const { theme } = useTheme()
   const router = useRouter()
 
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    setLoading(true)
-    const { email, password } = values
-    try {
-      const { error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-      })
-      if (error) {
-        setLoading(false)
-      } else {
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event) => {
+      if (event === 'SIGNED_IN') {
         router.replace('/')
       }
-    } catch (error) {
-      setLoading(false)
-    }
-  }
+    })
+  })
 
   return (
     <div className="flex-1 flex justify-center items-center">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input {...field} type="email" className="w-96" />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input {...field} type="password" className="w-96" />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <Button
-            variant="secondary"
-            className="w-full"
-            type="submit"
-            disabled={loading}
-          >
-            {loading ? 'Logining...' : 'Login'}
-          </Button>
-        </form>
-      </Form>
+      <div className="w-96 bg-background p-8 shadow-xl shadow-slate-100">
+        <Auth
+          supabaseClient={supabase}
+          providers={[]}
+          appearance={{ theme: ThemeSupa }}
+          theme={theme}
+        />
+      </div>
     </div>
   )
 }
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,2 @@
-export * from './button'
 export * from './command'
 export * from './dialog'
-export * from './form'
-export * from './input'
-export * from './label'
-export * from './tooltip'
