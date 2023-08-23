4d22c4271a410172ed1b8aad6d3cb4c08f3e9f9c
copofe <me@shinji.me>
2023-08-23 17:10:13 +0800

    feat: memos

--- src/app/(main)/memo/page.tsx
+++ src/app/(main)/memo/page.tsx
@@ -1,8 +1,42 @@
+import dayjs from 'dayjs'
+
+interface Memo {
+  id: number
+  rowStatus: string
+  creatorId: number
+  createdTs: number
+  updatedTs: number
+  displayTs: number
+  content: string
+  visibility: string
+  pinned: boolean
+  creatorName: string
+  creatorUsername: string
+  resourceList: any[]
+  relationList: any[]
+}
+
+const memosApi = process.env.MEMOS_API
+const openId = process.env.MEMOS_OPENID
+
 export default async function Memo() {
+  const res = await fetch(`${memosApi}?openId=${openId}`)
+  const memos: Memo[] = await res.json()
   return (
-    <div className="flex-1 flex justify-center items-center flex-col">
-      <div className="seaborn text-6xl">Coming soon...</div>
-      <div className="mt-12 text-muted-foreground">Coming soon...</div>
+    <div className="flex-1 self-center flex items-stretch flex-col w-[42rem] mt-8 lg:mt-12">
+      {memos.map((memo) => {
+        return (
+          <div
+            key={memo.id}
+            className="p-4 mb-4 rounded-lg transition-all bg-background border hover:shadow"
+          >
+            <div className="text-xs text-muted-foreground mb-2">
+              {dayjs(memo.createdTs * 1000).format('YYYY-MM-DD hh:mm:ss')}
+            </div>
+            <div>{memo.content}</div>
+          </div>
+        )
+      })}
     </div>
   )
 }
