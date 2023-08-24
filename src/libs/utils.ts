259b266d5774ff9fb9f931836af704ee34efe03d
copofe <me@shinji.me>
2023-08-24 12:38:52 +0800

    clean

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -4,10 +4,3 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export async function postRequest(url: string, { arg }: { arg: any }) {
-  return fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(arg)
-  })
-}
