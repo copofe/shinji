49386f8e55f3007a538ea617e34670666b240e76
copofe <me@shinji.me>
2023-08-24 12:41:32 +0800

    update fetch cache

--- src/app/(main)/memo/page.tsx
+++ src/app/(main)/memo/page.tsx
@@ -20,7 +20,7 @@ const memosApi = process.env.MEMOS_API
 const openId = process.env.MEMOS_OPENID
 
 export default async function Memo() {
-  const res = await fetch(`${memosApi}?openId=${openId}`, { cache: 'no-cache' })
+  const res = await fetch(`${memosApi}?openId=${openId}`, { next: { revalidate: 3600 } })
   const memos: Memo[] = await res.json()
   return (
     <div className="flex-1 self-center flex items-stretch flex-col w-full p-2 lg:p-0 lg:w-[42rem] mt-8 lg:mt-12">
