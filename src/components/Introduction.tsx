de96266e80433c797ee3d31ecaa9f34744925f71
copofe <me@shinji.me>
2023-09-02 20:20:54 +0800

    style

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
-
 'use client'
 
 import { motion } from 'framer-motion'
@@ -10,27 +8,27 @@ export default function Introduction() {
     <>
       <div className="p-4 md:px-40 md:py-20 flex-1 flex flex-col justify-center items-stretch self-stretch">
         <div className="overflow-hidden">
-          <div className="text-6xl md:text-9xl !leading-tight index-title">
+          <div className="text-6xl md:text-9xl !leading-tight move-up">
             Professional Developer
           </div>
         </div>
         <div className="overflow-hidden flex justify-end items-baseline">
-          <div className="text-xl font-thin mr-4">
+          <motion.div initial={{ opacity: 0, x: '15%' }} animate={{ opacity: 1, x: 0 }} transition={{ delay: 1.25, ease: 'linear', duration: 0.75 }} className="text-xl font-thin mr-4 text-gray-400">
             特に「侘び寂び」，原始的な美意識
-          </div>
-          <div className="text-6xl md:text-9xl !leading-tight index-title">
+          </motion.div>
+          <div className="text-6xl md:text-9xl !leading-tight move-up">
             Like Design
           </div>
         </div>
         <div className="overflow-hidden flex justify-center items-baseline">
-          <div className="text-6xl md:text-9xl !leading-tight index-title mr-4">
+          <div className="text-6xl md:text-9xl !leading-tight move-up mr-4">
             Also Gamer
           </div>
-          <div className="text-xl font-thin">玩游戏的都是朋友</div>
+          <motion.div initial={{ opacity: 0, x: '-15%' }} animate={{ opacity: 1, x: 0 }} transition={{ delay: 1.25, ease: 'linear', duration: 0.75 }} className="text-xl font-thin text-gray-400">嗨，玩游戏的都是朋友</motion.div>
         </div>
       </div>
       <div className="overflow-hidden flex justify-center my-4">
-        <div className="flex self-start [&>*]:mx-3 index-title">
+        <div className="flex self-start [&>*]:mx-3 move-up">
           <SocialLink platform="github" href="https://github.com/copofe" />
           <SocialLink platform="twitter" href="https://twitter.com/Shinji_Zl" />
           <SocialLink platform="mail" href="mailto:me@shinji.me" />
