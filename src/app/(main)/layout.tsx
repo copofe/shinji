d5e292da01c5f9bf28fa85ea4ac106cb4fa4da7e
copofe <me@shinji.me>
2023-11-29 20:21:04 +0800

    style

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import { useRef } from 'react'
 import { AnimatePresence } from 'framer-motion'
-import Cursor from '::/components/Cursor'
 import Footer from '::/components/Footer'
 import Header from '::/components/Header'
 
@@ -11,16 +9,14 @@ export default function MainLayout({
 }: {
   children: React.ReactNode
 }) {
-  const ref = useRef(null)
   return (
     <AnimatePresence>
-      <div className="flex-1 flex flex-col items-stretch relative z-10" ref={ref}>
+      <div className="flex-1 flex flex-col items-stretch relative z-10">
         <Header key="header" />
         <main key="main" className="flex-1 flex flex-col w-full">
           {children}
         </main>
         <Footer />
-        <Cursor refEle={ref} />
       </div>
     </AnimatePresence>
   )
