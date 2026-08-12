bd3e88c8a6bba091a09d28eed3bfa2d64952aeab
Shinji <me@shinji.me>
2026-08-12 11:53:06 +0800

    chore: 简介文案标点修正

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -5,7 +5,7 @@ export default function Introduction() {
     <header className="mb-16 mt-8 md:mb-32 md:mt-8 flex flex-col items-start">
       <h1 className="text-base md:text-lg font-medium leading-tight">@Shinji</h1>
       <div className="mt-2 text-sm md:text-base text-muted-foreground leading-relaxed">
-        Professional Developer, Love Design. Also a gamer.
+        Professional Developer. Love Design. Also a gamer.
       </div>
       <div className="mt-6 flex *:mr-4">
         <SocialLink platform="github" href="https://github.com/copofe" />
